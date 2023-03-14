30b435d14c3fc1bdc94da88aadaffa2a6e0eb540
Shiroe <maciejbielecki99@gmail.com>
2023-03-14 22:29:46 +0100

    tkinter gui added

--- gpt4_gui.py
+++ gpt4_gui.py
@@ -1,3 +1,8 @@
+import threading
+# Add your other imports here
+# ...
+import tkinter as tk
+from tkinter import ttk
 import speech_recognition as sr
 import pyttsx3
 import time 
@@ -13,17 +18,12 @@ import chatgpt_api
 import request_voice_tts as request_voice
 import sys
 from db_config import conn
-import redis
-
-redis_client = redis.Redis(host='localhost', port=6379)
-
-
-    
+from tkinter import scrolledtext
 
 # Initialize the question to speech engine 
 engine=pyttsx3.init()
 conn = None
-transcription = ""
+
 
 def transcribe_audio_question(filename):
     start_time = time.time()
@@ -67,22 +67,19 @@ def play_audio_fn(filename):
     stream.close()
     p.terminate()
     print("Playing voice")
+# Replace print() statements with this function
+def print_response(response):
+    response_area.insert(tk.END, response + '\n')
+    response_area.see(tk.END)
+def print_response_label(response):
+    response_label.config(text=response)
 
-
-
-def main():
-    #input your name
-    print("say 'exit program' to exit the program")
-    print("What is your name?: ")
-    name = input()
-    print("Do you want to reset your chat history? (yes/no):")
-    choice = input()
-    if choice =="yes":
-        connect_to_phpmyadmin.reset_chat_history(name)
+def voice_control():
+    recognizer = sr.Recognizer() # don't knwo why but without this it doesn't work probably because : with sr.Microphone() as source:
+    print("started lintening")
+    while running: #while runnign is true
         
-    connect_to_phpmyadmin.check_user_in_database(name)
-    
-    while True:
+        print_response("Say 'pathfinder' to start a conversation")
         messages = connect_to_phpmyadmin.retrieve_chat_history_from_database(name)
         #Wait for user to say "pathfinder"
         print("Say 'pathfinder' to start a conversation")
@@ -96,7 +93,8 @@ def main():
             f.close()  
             try:
                 transcription = transcribe_audio_question(alias) #make transcription from alias file
-                redis_client.set('mykey1', transcription)
+                print_response(transcription)
+                print_response_label(transcription)
                 if transcription.rstrip('.,?!').lower() == " exit program":
                     sys.exit()
                 if transcription.rstrip('.,?!').replace(' ', '').lower() == "pathfinder":
@@ -122,17 +120,12 @@ def main():
                         question = f"{name}: {question}"
                             #add question line to messages list
                         messages.append({"role": "user", "content": question})
-
-                        # send to open ai for answer
-                        answer, prompt_tokens, completion_tokens, total_tokens = chatgpt_api.send_to_openai(messages)
-                        
-                        
-                        
+                            # send to open ai for answer
+                        answer, prompt_tokens, completion_tokens, total_tokens = chatgpt_api.send_to_openai(messages)        
                         request_voice.request_voice_fn(answer) #request Azure TTS to for answer
                         
                         print("ShiroAi-chan: " + answer)
                         play_audio_fn("response")
-                        
                         
                         if profanity.contains_profanity(answer) == True:
                             answer = profanity.censor(answer)                    
@@ -141,12 +134,60 @@ def main():
                         connect_to_phpmyadmin.add_pair_to_general_table(name, answer) #to general table with all  questions and answers
                         connect_to_phpmyadmin.send_chatgpt_usage_to_database(prompt_tokens, completion_tokens, total_tokens) #to Azure DB with usage stats
                         print("---------------------------------")
-                        
-                
 
-
+                        if transcribed == " exit program":
+                            print("exiting program")
+                            sys.exit()
             except Exception as e:
                 print("An error occurred: {}".format(e))
+    # Replace `print()` statements with `print_response()`
+            # ...
 
-if __name__ == "__main__":
-    main()
+print("say 'exit program' to exit the program")
+print("What is your name?: ")
+name = input()
+print("Do you want to reset your chat history? (yes/no):")
+choice = input()
+if choice =="yes":
+    connect_to_phpmyadmin.reset_chat_history(name)
+    
+connect_to_phpmyadmin.check_user_in_database(name)
+
+def start_voice_control():
+    global running
+    running = True #will start the while loop in voice control
+    voice_thread = threading.Thread(target=voice_control)
+    voice_thread.start()
+
+
+def stop_listening():
+    global running
+    running = False #will cut off the while loop in voice control
+    
+
+# GUI elements
+root = tk.Tk()
+root.title("ShiroAi-chan Control Panel")
+
+frame = tk.Frame(root)
+frame.pack(padx=10, pady=10)
+
+start_button = tk.Button(frame, text="Start Listening", command=start_voice_control, width=20)
+start_button.grid(row=0, column=0, padx=5, pady=5)
+
+stop_button = tk.Button(frame, text="Stop Listening", command=stop_listening)
+stop_button.grid(row=0, column=1, padx=5, pady=5)
+
+# other_button = tk.Button(frame, text="Other Function", command=other_function)
+# other_button.grid(row=0, column=2, padx=5, pady=5)
+
+response_area = scrolledtext.ScrolledText(frame, wrap=tk.WORD, width=60, height=10)
+response_area.grid(row=1, column=0, columnspan=3, padx=5, pady=5)
+
+response_label = tk.Label(root, text="", wraplength=300)
+response_label.pack()
+
+running = False
+thread_running = False
+
+root.mainloop()
