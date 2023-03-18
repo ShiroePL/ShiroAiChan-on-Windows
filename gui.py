fefbf0da385b928bf5cea75c1281b43639073ff9
Shiroe <maciejbielecki99@gmail.com>
2023-03-18 02:21:07 +0100

    big changes, new ui

--- gui.py
+++ gui.py
@@ -20,15 +20,15 @@ def relative_to_assets(path: str) -> Path:
 
 window = Tk()
 
-window.geometry("1000x600")
+window.geometry("1200x800")
 window.configure(bg = "#4B98E0")
 
 
 canvas = Canvas(
     window,
     bg = "#4B98E0",
-    height = 600,
-    width = 1000,
+    height = 800,
+    width = 1200,
     bd = 0,
     highlightthickness = 0,
     relief = "ridge"
@@ -38,18 +38,26 @@ canvas.place(x = 0, y = 0)
 image_image_1 = PhotoImage(
     file=relative_to_assets("image_1.png"))
 image_1 = canvas.create_image(
-    500.0,
-    300.0,
+    600.0,
+    400.0,
     image=image_image_1
 )
 
+image_image_2 = PhotoImage(
+    file=relative_to_assets("image_2.png"))
+image_2 = canvas.create_image(
+    959.0,
+    400.0,
+    image=image_image_2
+)
+
 canvas.create_text(
-    204.0,
-    7.0,
+    257.0,
+    12.0,
     anchor="nw",
     text="ShiroAi-chan Control Panel",
-    fill="#F9C6B3",
-    font=("Inter Bold", 40 * -1)
+    fill="#12D4FF",
+    font=("Inter Bold", 48 * -1)
 )
 
 button_image_1 = PhotoImage(
@@ -62,10 +70,10 @@ button_1 = Button(
     relief="flat"
 )
 button_1.place(
-    x=64.0,
-    y=69.0,
-    width=120.0,
-    height=50.0
+    x=52.0,
+    y=91.0,
+    width=127.0,
+    height=51.0
 )
 
 button_image_2 = PhotoImage(
@@ -78,47 +86,119 @@ button_2 = Button(
     relief="flat"
 )
 button_2.place(
-    x=225.0,
-    y=66.0,
-    width=120.0,
-    height=53.0
+    x=204.0,
+    y=91.0,
+    width=127.0,
+    height=51.0
+)
+
+button_image_3 = PhotoImage(
+    file=relative_to_assets("button_3.png"))
+button_3 = Button(
+    image=button_image_3,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_3 clicked"),
+    relief="flat"
+)
+button_3.place(
+    x=356.0,
+    y=91.0,
+    width=127.0,
+    height=51.0
+)
+
+button_image_4 = PhotoImage(
+    file=relative_to_assets("button_4.png"))
+button_4 = Button(
+    image=button_image_4,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_4 clicked"),
+    relief="flat"
+)
+button_4.place(
+    x=508.0,
+    y=91.0,
+    width=127.0,
+    height=51.0
+)
+
+button_image_5 = PhotoImage(
+    file=relative_to_assets("button_5.png"))
+button_5 = Button(
+    image=button_image_5,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_5 clicked"),
+    relief="flat"
+)
+button_5.place(
+    x=52.0,
+    y=165.0,
+    width=127.0,
+    height=51.0
+)
+
+button_image_6 = PhotoImage(
+    file=relative_to_assets("button_6.png"))
+button_6 = Button(
+    image=button_image_6,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_6 clicked"),
+    relief="flat"
+)
+button_6.place(
+    x=204.0,
+    y=165.0,
+    width=127.0,
+    height=51.0
+)
+
+button_image_7 = PhotoImage(
+    file=relative_to_assets("button_7.png"))
+button_7 = Button(
+    image=button_image_7,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_7 clicked"),
+    relief="flat"
+)
+button_7.place(
+    x=356.0,
+    y=165.0,
+    width=127.0,
+    height=51.0
 )
 
 entry_image_1 = PhotoImage(
     file=relative_to_assets("entry_1.png"))
 entry_bg_1 = canvas.create_image(
-    124.5,
-    168.0,
+    113.0,
+    52.0,
     image=entry_image_1
 )
 entry_1 = Entry(
     bd=0,
-    bg="#FFDDD3",
+    bg="#7FD7EC",
     fg="#000716",
     highlightthickness=0
 )
 entry_1.place(
-    x=53.0,
-    y=151.0,
-    width=143.0,
+    x=52.0,
+    y=35.0,
+    width=122.0,
     height=32.0
 )
 
 canvas.create_text(
-    31.0,
-    119.0,
+    58.0,
+    5.0,
     anchor="nw",
-    text="name:",
-    fill="#FFFFFF",
-    font=("Inter Regular", 24 * -1)
-)
-
-image_image_2 = PhotoImage(
-    file=relative_to_assets("image_2.png"))
-image_2 = canvas.create_image(
-    810.0,
-    312.0,
-    image=image_image_2
+    text="Your name:",
+    fill="#7FD5EA",
+    font=("Inter Bold", 20 * -1)
 )
 window.resizable(False, False)
 window.mainloop()
