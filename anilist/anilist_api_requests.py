6bb002c31c673043f11754891c35a255d20bc980
Shiroe <maciejbielecki99@gmail.com>
2023-05-20 15:26:22 +0200

    added manga version

--- anilist/anilist_api_requests.py
+++ anilist/anilist_api_requests.py
@@ -72,6 +72,37 @@ def change_episodes_watched(ANIME_ID: int, EPISODES_WATCHED: int):
   else:
       print("Anime status updated successfully!")
 
+def change_chapters_count(MANGA_ID: int, CHAPTERS_READ: int):
+  """Update the number of episodes watched for an manga/novel"""
+  headers = {'Authorization': f'Bearer {access_token}', 'Content-Type': 'application/json'}
+
+  # Replace 'ANIME_ID' with the ID of the anime you want to update, and 'NEW_STATUS' with the desired status
+      # for testing
+#   ANIME_ID = 6164  # Replace this with the actual anime ID
+#   EPISODES_WATCHED = 6  # Replace this with the desired episodes watched
+
+  query = '''
+  mutation ($id: Int, $chapters: Int) {
+    SaveMediaListEntry (mediaId: $id, progress: $chapters) {
+      id
+      progress
+    }
+  }
+  '''
+
+  variables = {'id': MANGA_ID, 'chapters': CHAPTERS_READ}
+
+  response = requests.post('https://graphql.anilist.co', json={'query': query, 'variables': variables}, headers=headers)
+  if response.status_code != 200:
+      print(response.content)
+
+  response.raise_for_status()
+  data = response.json()
+
+  if 'errors' in data:
+      print(f"An error occurred: {data['errors']}")
+  else:
+      print("Manga/Novel status updated successfully!")
 
 
 
@@ -79,8 +110,8 @@ def get_10_newest_anime():
   """Get the 10 newest anime formatted for prompt"""
   
   variables_in_api = {
-    'page' : 1
-    }
+      'page' : 1
+      }
   
   formatted_10_list = ""
 
@@ -152,12 +183,105 @@ def get_10_newest_anime():
   return formatted_10_list, newest_10_anime
 
 
-def find_anime_by_id(anime_list, anime_id):
-    for anime in anime_list:
-        if anime['mediaId'] == anime_id:
-            return anime
-    return None
+def get_10_newest_entries(media_type: str):
+  """Get the 10 newest anime/manga formatted for prompt
+  type: 'ANIME' or 'MANGA'"""
+  
+  if media_type == 'ANIME':
+    episodes_or_chapters = 'episodes'
+  elif media_type == 'MANGA':
+    episodes_or_chapters = 'chapters'
 
+  variables_in_api = {
+    'page' : 1,
+    'type' : media_type,
+    'episodes_or_chapters' : episodes_or_chapters,
+    }
+
+  formatted_10_list = ""
+
+  api_request = '''
+    query ($page: Int, $type: MediaType) {
+  Page(page: $page, perPage: 10) {
+    pageInfo {
+      perPage
+    }
+    mediaList(userId: 444059, type: $type, sort: UPDATED_TIME_DESC) {
+      mediaId
+      status
+      progress
+      updatedAt
+      media {
+        title {
+          romaji
+          english
+        }
+        episodes
+        chapters
+      }
+    }
+  }
+}'''
+    
+  url = 'https://graphql.anilist.co'
+
+  response = requests.post(url, json={'query': api_request, 'variables': variables_in_api})
+
+  response.raise_for_status()
+
+  parsed_json = json.loads(response.text)
+
+  newest_10_entries = []
+  
+  j = 0
+  for j in range(len(parsed_json["data"]["Page"]["mediaList"])):
+
+    media_list = parsed_json["data"]["Page"]["mediaList"][j]
+    media = media_list["media"]
+    title = media["title"]
+
+    media_dict = {
+    'on_list_status': media_list["status"],
+    'mediaId': media_list["mediaId"],
+    'progress': media_list["progress"],
+    'updatedAt': media_list["updatedAt"],
+    'english': (title["english"].replace("'", '"') if title["english"] is not None else "no english title"),
+    'romaji': title["romaji"].replace("'", '"')
+    }
+
+    if media_type == 'ANIME':
+        media_dict['episodes'] = media["episodes"] or 0
+    elif media_type == 'MANGA':
+        media_dict['chapters'] = media["chapters"] or 0
+
+    newest_10_entries.append(media_dict)
+
+  for media in newest_10_entries:
+      
+      if media_type == 'ANIME':
+        title = media['romaji'].replace('’', "'")
+        title = title.replace('"', "'")
+        formatted_10_list += f"\nromaji_title:{title}, id:{media['mediaId']}, watched_episodes:{media['progress']}/{media['episodes']} "
+
+      elif media_type == 'MANGA':
+        title = media['english'].replace('’', "'")
+        title = title.replace('"', "'") 
+        formatted_10_list += f"\nromaji_title:{title}, id:{media['mediaId']}, read_chapters:{media['progress']}/{media['chapters']} "
+
+      
+
+    
+
+  return formatted_10_list, newest_10_entries
+
+
+
+
+def find_media_by_id(media_list, media_id):
+    for media in media_list:
+        if media['mediaId'] == media_id:
+            return media
+    return None
 
 
 
@@ -166,9 +290,9 @@ if __name__ == "__main__":
     
     # change_episodes_watched(6164, 6)
     # change_anime_status(6164, 'PAUSED')
-    formatted_list, raw_list = get_10_newest_anime()
-    search_id = 127550
-    found_anime = find_anime_by_id(raw_list, search_id)
+    formatted_list, raw_list = get_10_newest_entries("MANGA")
+    search_id = 113061
+    found_anime = find_media_by_id(raw_list, search_id)
     print ("formatted list: ", formatted_list)
     if found_anime:
         print(f"Found anime with ID {search_id}:")
