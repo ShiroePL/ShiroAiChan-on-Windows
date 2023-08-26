868f33cfd1062db432d26eebbab9035713dead76
Shiroe <maciejbielecki99@gmail.com>
2023-08-26 21:08:52 +0200

    room temperature in home assistant

---
+++ home_assistant/ha_api_requests.py
@@ -0,0 +1,24 @@
+import requests
+import home_assistant.api_keys as api_keys
+
+# token = api_keys.token
+# server_ip = api_keys.server_ip
+
+def room_temp():
+    token = api_keys.token
+    server_ip = api_keys.server_ip
+    url = f"http://{server_ip}:8123/api/states/sensor.living_room_temperature_2"
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        temperature = response.json()['state']
+        print(f"The living room temperature is {temperature}°C")
+    else:
+        print(f"Failed to get temperature: {response.content}")
+    return temperature
+#room_temp(token, server_ip)
