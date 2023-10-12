fc4daeeaa74eae7842b0828d7486e5c412ef6d50
Shiroe <maciejbielecki99@gmail.com>
2023-10-12 19:56:12 +0200

    fix

--- kiki_hub/request_voice_tts.py
+++ kiki_hub/request_voice_tts.py
@@ -1,8 +1,9 @@
-#For more samples please visit https://github.com/Azure-Samples/cognitive-services-speech-sdk 
+
 import os
 import azure.cognitiveservices.speech as speechsdk
 
-def request_voice_fn(text):
+
+def request_voice_fn(text, lang=False):
         #speech config
     speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
     
@@ -10,8 +11,14 @@ def request_voice_fn(text):
 
     ssml = """<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US">
         <voice name="en-US-AshleyNeural"><prosody rate="3%" pitch="21%">""" + text + """</prosody></voice></speak>"""
-   
-    result = synthesizer.speak_ssml_async(ssml).get()
+    
+    ssml_pl = """<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="pl-PL">
+    <voice name="pl-PL-ZofiaNeural"><prosody rate="+7.00%" volume="-60.00%" pitch="+7.00%">""" + text + """</prosody></voice></speak>"""
+
+    if lang ==True:
+        result = synthesizer.speak_ssml_async(ssml_pl).get()
+    else:  
+        result = synthesizer.speak_ssml_async(ssml).get()
 
     stream = speechsdk.AudioDataStream(result)
     stream.save_to_wav_file("./kiki_hub/response.wav")
